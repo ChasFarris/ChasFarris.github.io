fc1c61f097f1904eac516f7eebb056fb52366de7
ChasFarris <ChasFarrisWork@gmail.com>
2017-04-11 23:31:03 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,4 +1,5 @@
-/* DropDown */
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
