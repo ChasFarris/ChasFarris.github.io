fb000a8faa22fa912fc07fa03a313d2ea85537b0
ChasFarris <ChasFarrisWork@gmail.com>
2017-04-11 23:03:20 -0500

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,25 @@
+/* DropDown */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/* Time*/
+var text = document.createTextNode('Today is:');
+var d = new Date();
+document.getElementById("demo").innerHTML = d.toDateString();
+demo.parentNode.insertBefore(text, demo);
