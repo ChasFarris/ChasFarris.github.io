e1252d28fa4b5e9edda9317caba56bcbce241570
ChasFarris <ChasFarrisWork@gmail.com>
2017-04-25 16:45:10 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -18,3 +18,13 @@ window.onclick = function(event) {
     }
   }
 }
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
