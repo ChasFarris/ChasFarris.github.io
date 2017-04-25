72fb6a89295ef5b8514a1b2f85871f3e5774f6e3
ChasFarris <ChasFarrisWork@gmail.com>
2017-04-25 18:28:26 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -19,13 +19,11 @@ window.onclick = function(event) {
   }
 }
 
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-}, 3000);
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      3000);
+});
